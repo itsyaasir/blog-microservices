853444562b9b5299d78fe6b8e293ccf819eaeab9
Yasir Shariff <goodwonder5@gmail.com>
2021-01-29 19:20:05 +0000

    deploying of posts and event services

--- posts/index.js
+++ posts/index.js
@@ -23,7 +23,7 @@ app.post("/posts", async (req, res) => {
     title,
   };
 
-  await axios.post("http://localhost:4005/events", {
+  await axios.post("http://event-bus-srv:4005/events", {
     type: "PostCreated",
     data: {
       id,
@@ -41,6 +41,6 @@ app.post("/events", (req, res) => {
 });
 
 app.listen(4000, () => {
-  console.log("v20");
+  console.log("v55");
   console.log("Listening on 4000");
 });
