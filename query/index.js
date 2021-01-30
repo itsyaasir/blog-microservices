2e46a227bcb728e32cd20594389803846ae7af16
Yasir Shariff <goodwonder5@gmail.com>
2021-01-30 18:15:29 +0000

    adding inginx-ingress for load-balancing and routing services

--- query/index.js
+++ query/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
@@ -10,24 +10,24 @@ app.use(cors());
 const posts = {};
 
 const handleEvent = (type, data) => {
-  if (type === 'PostCreated') {
+  if (type === "PostCreated") {
     const { id, title } = data;
 
     posts[id] = { id, title, comments: [] };
   }
 
-  if (type === 'CommentCreated') {
+  if (type === "CommentCreated") {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
     post.comments.push({ id, content, status });
   }
 
-  if (type === 'CommentUpdated') {
+  if (type === "CommentUpdated") {
     const { id, content, postId, status } = data;
 
     const post = posts[postId];
-    const comment = post.comments.find(comment => {
+    const comment = post.comments.find((comment) => {
       return comment.id === id;
     });
 
@@ -36,11 +36,11 @@ const handleEvent = (type, data) => {
   }
 };
 
-app.get('/posts', (req, res) => {
+app.get("/posts", (req, res) => {
   res.send(posts);
 });
 
-app.post('/events', (req, res) => {
+app.post("/events", (req, res) => {
   const { type, data } = req.body;
 
   handleEvent(type, data);
@@ -49,12 +49,12 @@ app.post('/events', (req, res) => {
 });
 
 app.listen(4002, async () => {
-  console.log('Listening on 4002');
+  console.log("Listening on 4002");
 
-  const res = await axios.get('http://localhost:4005/events');
+  const res = await axios.get("http://event-bus-srv:4005/events");
 
   for (let event of res.data) {
-    console.log('Processing event:', event.type);
+    console.log("Processing event:", event.type);
 
     handleEvent(event.type, event.data);
   }
